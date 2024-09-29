6175d6710e7136b25993b87344506c17bea0ce1c
alex cesar <thealexcesar@gmail.com>
2024-09-29 18:18:18 -0300

    ✨ feat: initial commit

---
+++ src/main/java/com/exploredenmarkapi/ExploreDenmarkApiApplication.java
@@ -0,0 +1,13 @@
+package com.exploredenmarkapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExploreDenmarkApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExploreDenmarkApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/exploredenmarkapi/ExploreDenmarkApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exploredenmarkapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExploreDenmarkApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
